3cd72c1edf5e58df9cf49ce110e178527b595bf4
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2025-12-17 18:53:55 +0100

    feat: Amélioration des cartes UI pour chaque demande avec des informations détaillées sur l'état des formulaires

--- src/lib/ui/Follow.svelte
+++ src/lib/ui/Follow.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
+  import { DateTime } from 'luxon';
+
   import HorizontalScroller from '$lib/ui/HorizontalScroller.svelte';
   import SectionTile from '$lib/ui/SectionTile.svelte';
   import CardItem from '$lib/ui/CardItem.svelte';
-  import { DateTime } from 'luxon';
 
   let { requests } = $props();
-  let url = undefined;
   let showHeading = $state(false);
 </script>
 
@@ -16,13 +16,69 @@
     bind:showHeading
   >
     <SectionTile title={!showHeading ? 'Suivre' : ''} />
-    {#each requests as request}
+    {#each requests as request (request.record_id)}
       <div class="flex-shrink-0">
         <CardItem>
           {#snippet description()}
             Ma demande {request.record_id} est en cours de traitement.
           {/snippet}
-          {#snippet actions()}{/snippet}
+          {#snippet actions()}
+            <div class="list-group-item list-group-item-{request.form_complete === '2' ? 'success' : 'warning'}">
+              {request.form_complete === '2' ? 'Mon formulaire est complet' : 'Je dois compléter mon formulaire'}.
+            </div>
+            <div
+              class="list-group-item list-group-item-{request.form_complete !== '2'
+                ? 'info'
+                : request.composante_complete === '2'
+                  ? 'success'
+                  : 'warning'}"
+            >
+              Ma composante {request.form_complete !== '2'
+                ? 'attend mon formulaire'
+                : request.composante_complete === '2'
+                  ? 'a informé sa décision'
+                  : 'se concerte'}.
+            </div>
+            <div
+              class="list-group-item list-group-item-{request.form_complete !== '2'
+                ? 'info'
+                : request.labo_complete === '2'
+                  ? 'success'
+                  : 'warning'}"
+            >
+              Mon laboratoire {request.form_complete !== '2'
+                ? 'attend mon formulaire'
+                : request.labo_complete === '2'
+                  ? 'a informé sa décision'
+                  : 'se concerte'}.
+            </div>
+            <div
+              class="list-group-item list-group-item-{request.form_complete !== '2'
+                ? 'info'
+                : request.encadrant_complete === '2'
+                  ? 'success'
+                  : 'warning'}"
+            >
+              Mon encadrant {request.form_complete !== '2'
+                ? 'attend mon formulaire'
+                : request.encadrant_complete === '2'
+                  ? 'a informé sa décision'
+                  : 'se concerte'}.
+            </div>
+            <div
+              class="list-group-item list-group-item-{request.form_complete !== '2'
+                ? 'info'
+                : request.validation_finale_complete === '2'
+                  ? 'success'
+                  : 'warning'}"
+            >
+              {request.form_complete !== '2'
+                ? "Ma validation finale n'est pas disponible tant que mon formulaire n'est pas complet"
+                : request.validation_finale_complete === '2'
+                  ? 'Ma validation finale est complète'
+                  : 'Je dois compléter ma validation finale'}.
+            </div>
+          {/snippet}
           {#snippet footer()}
             <small class="text-body-secondary">
               Créée {DateTime.fromISO(request.created_at).toRelative({ locale: 'fr' })}
